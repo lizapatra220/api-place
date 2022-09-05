f6f7170d9d0c3629a54aa5565848561b9da45ebe
lizapatra220 <lizapatra220@gmail.com>
2022-09-05 11:57:34 +0530

    api-place

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import axios from 'axios';
+import  './App.css';
+import React, { useEffect,useState } from 'react'
+import axios from 'axios';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [countries, setcountries] = useState([]);
+  const [singlecountry, setsinglecountry] = useState("");
+  const [cities, setcities] = useState(null);
+  const [singlecity, setsinglecity] = useState("");
+  const [submit, setsubmit] = useState(false);
+  const fetchcountry=async()=>
+  {
+    try {
+      const country= await axios.get('https://countriesnow.space/api/v0.1/countries');
+      // console.log(country.data.data);
+      setcountries(country.data.data);
+    } catch (error) {
+      console.log(error);
+   
+    }
+  }
+  const fetchcities =(country)=>{
+    setsubmit(false);
+    setsinglecity();
+    setsinglecountry(country);
+    const findcities=countries.find((t)=>t.country==country);
+    setcities(findcities.cities);
+
+  }
+  const handlesubmit =()=>{
+    if(setsinglecountry &&setsinglecity)
+    setsubmit(true);
+  }
+  useEffect(() => {
+    fetchcountry();
+
+  },[] )
+  
+  return<div  className='App'>
+  <div className='app-header'>
+    <h1>Select Your Hometown</h1>
+    <div>
+      {countries && (
+          <select onChange={(e)=>fetchcities(e.target.value)}>
+          <option disabled selected hidden>
+            select country
+            </option>
+            {countries.map((country)=>(
+              <option key={'${country.country}-${date.now()}'} value={country.country}>{country.country}</option>
+
+            ))}
+        </select>
+      ) }
+      {cities &&(
+         <select onChange={(e)=>setsinglecity(e.target.value) } >
+         <option disabled selected hidden>
+           select city
+           </option>
+           {cities.map((city)=>(
+            <option key={city} value={city}>{city}</option>
+
+           ))}
+  
+       </select>
+      )
+      }
+
+      <button onClick={handlesubmit}>Go</button>
     </div>
-  );
+    {
+      submit && <h3>
+      Your Country is {singlecountry} and Your City is {singlecity}.
+    </h3>
+    }
+    
+  </div>
+  </div>
+  
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   // const [count, setcount] = useState(0);
+//   const [userDetails, setuserDetails] = useState(0);
+//  const user = async()=>{
+//   try {
+//     const userDetails = await
+//      axios.get("https://randomuser.me/api/");
+//     //  console.log(userDetails);
+//     setuserDetails(userDetails.data.results[0]);
+
+    
+//   } catch (error) {
+    
+//   }
+
+//  }
+//  console.log(userDetails);
+
+
+//   useEffect(() => {
+//     // setcount(count+5);
+//     user();
+  
+//   },[] )
+  
+//   return<div  className='App'>
+// {userDetails && <img src={userDetails.picture.large}/>};
+  
+//   </div>
+  
+// }
+
